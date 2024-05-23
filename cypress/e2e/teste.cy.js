cb3785e4994ac73bb30383b15137f85544700e37
Juliana Martins <jullianamartins@gmail.com>
2024-05-23 09:20:42 -0300

    agora vai

---
+++ cypress/e2e/teste.cy.js
@@ -0,0 +1,14 @@
+describe('Página Principal', () => {
+
+    it('deve acessar a página principal e clicar em um botão', () => {
+      // Visita a página principal (substitua 'http://localhost:3000' pelo URL correto)
+      cy.visit('http://localhost:3000');
+  
+      // Clica no botão (substitua 'seletor-do-botao' pelo seletor correto do botão)
+     // cy.get('Acesse aqui').click();
+  
+      // Adicione asserções para verificar se o clique no botão teve o efeito esperado
+      // Por exemplo, verificar se uma nova página foi carregada, um modal abriu, etc.
+      // cy.url().should('include', '/nova-pagina'); // Exemplo de asserção de URL
+    });
+  });
